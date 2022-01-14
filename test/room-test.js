7d93dcaec0e5f9c2eed1e983402b81eecfe114f6
Richard Rosa-Serrano <richardjrosaserrano@gmail.com>
2022-01-13 19:28:21 -0500

    test to see if the room contains a room number

--- test/room-test.js
+++ test/room-test.js
@@ -8,8 +8,13 @@ describe('Room', () => {
   beforeEach(() => {
     room = new Room(roomData[0])
   })
-  
+
   it('Should be a function', () => {
     expect(Room).to.be.a('function');
   })
+
+  it('Should have a number for the room', () => {
+    expect(room.number).to.equal(roomData[0].number)
+  })
+  
 })
