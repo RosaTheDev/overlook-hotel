853c63abf2732e9e4f8fa17d93f3eb06d14e416a
Richard Rosa-Serrano <richardjrosaserrano@gmail.com>
2022-01-13 19:32:19 -0500

    test to see if the room contains a bidet, also had semicolons to end of lines

--- test/room-test.js
+++ test/room-test.js
@@ -5,19 +5,26 @@ import Room from '../src/classes/room'
 
 describe('Room', () => {
   let room;
+
   beforeEach(() => {
     room = new Room(roomData[0])
-  })
+  });
 
   it('Should be a function', () => {
     expect(Room).to.be.a('function');
-  })
+  });
 
   it('Should have a number for the room', () => {
     expect(room.number).to.equal(roomData[0].number)
-  })
+  });
 
   it('Should have a room type', () => {
     expect(room.roomType).to.equal(roomData[0].roomType)
+  });
+
+  it('should check to see if the room has a bidet', () => {
+    expect(room.hasBidet).to.equal(roomData[0].bidet)
   })
+
+
 })
