34d5dae6e45e89a2989a5dbdbe58b910f3d53095
Richard Rosa-Serrano <richardjrosaserrano@gmail.com>
2022-01-13 20:03:53 -0500

    Create a Customer test and check the class is a function

---
+++ test/customer-test.js
@@ -0,0 +1,16 @@
+import { expect } from 'chai';
+import customerData from './test-data/customer-data';
+import Customer from '../src/classes/customer';
+
+
+describe('Customer', () => {
+  let customer;
+
+  beforeEach(() => {
+    customer = new Customer(customerData[0])
+  });
+
+  it('Should be a function', () => {
+    expect(Customer).to.be.a('function');
+  });
+});
