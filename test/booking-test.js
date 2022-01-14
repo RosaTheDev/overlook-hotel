3f82b06b2197605e567a62b2c7880355048a5baf
Richard Rosa-Serrano <richardjrosaserrano@gmail.com>
2022-01-13 19:09:17 -0500

    Fix some indentation issues

--- test/booking-test.js
+++ test/booking-test.js
@@ -4,32 +4,32 @@ import Booking from '../src/classes/booking'
 
 
 describe('Booking', () => {
-    let booking;
-    beforeEach(() => {
-        booking = new Booking(bookingData[0])
-    })
+  let booking;
+  beforeEach(() => {
+    booking = new Booking(bookingData[0])
+  })
 
-    it('Should be a function', () => {
-        expect(Booking).to.be.a('function');
-    })
+  it('Should be a function', () => {
+    expect(Booking).to.be.a('function');
+  })
 
-    it('Should be able to hold an id', () => {
-        expect(booking.id).to.equal(bookingData[0].id)
-    })
+  it('Should be able to hold an id', () => {
+    expect(booking.id).to.equal(bookingData[0].id)
+  })
     
-    it('Should be able to hold a userID', () => {
-        expect(booking.userID).to.equal(bookingData[0].userID)
-    })
+  it('Should be able to hold a userID', () => {
+    expect(booking.userID).to.equal(bookingData[0].userID)
+  })
 
-    it('Should be able to hold onto a date', () => {
-        expect(booking.date).to.equal(bookingData[0].date)
-    })
+  it('Should be able to hold onto a date', () => {
+    expect(booking.date).to.equal(bookingData[0].date)
+  })
 
-    it('Should be able to hold a room number', () => {
-        expect(booking.roomNumber).to.equal(bookingData[0].roomNumber)
-    })
+  it('Should be able to hold a room number', () => {
+    expect(booking.roomNumber).to.equal(bookingData[0].roomNumber)
+  })
 
-    it('Should be able to hold onto room service charges', () => {
-        expect(booking.roomServiceCharges).to.equal(bookingData[0].roomServiceCharges)
-    })
+  it('Should be able to hold onto room service charges', () => {
+    expect(booking.roomServiceCharges).to.equal(bookingData[0].roomServiceCharges)
+  })
 })
